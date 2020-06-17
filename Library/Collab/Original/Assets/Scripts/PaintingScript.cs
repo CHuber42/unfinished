10ca5b625c34557fb9f1a8ce6ae1c3d27ce0fbb1
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-16 17:19:22 -0700

    fix hidden door entrance

--- Library/Collab/Original/Assets/Scripts/PaintingScript.cs
+++ Library/Collab/Original/Assets/Scripts/PaintingScript.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class PaintingScript : MonoBehaviour
 {
-    public SpriteRenderer WallSprite;
-    public Sprite replacementSprite;
-    // Start is called before the first frame update
-    void Start()
+  public PlayerController PlayerScript;
+  public SpriteRenderer WallSprite;
+  public Sprite replacementSprite;
+  public AudioSource soundEffect;
+  // Start is called before the first frame update
+  void Start()
+  {
+    PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    if (PlayerScript.paintingFlipped == true)
     {
-        
+      PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+      if (PlayerScript.gameState > 0)
+      {
+        WallSprite.sprite = replacementSprite;
+      }
+
+      WallSprite.sprite = replacementSprite;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 
-    void OnTriggerStay2D(Collider2D other)
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void OnTriggerStay2D(Collider2D other)
+  {
+    if (other.tag == "Player")
     {
-        if (other.tag == "Player")
+      if (other.tag == "Player")
+      {
+        if (Input.GetKeyDown("j"))
         {
-            if (Input.GetKeyDown("j"))
-            {
-              WallSprite.sprite = replacementSprite;
-            }
+          var player = other.GetComponentInParent<PlayerController>();
+          player.gameState += 1;
+          WallSprite.sprite = replacementSprite;
         }
+      }
     }
+  }
 }
