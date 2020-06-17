e9b65bdda5e21a10aedbb39ab67b89082ee884f5
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-17 15:04:28 -0700

    add upstairs cutscene and key logic

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class AreaExit : MonoBehaviour
 {
+  public bool lockedMessageTimer;
+  public AudioSource keyFail;
+  public AudioSource keySuccess;
+  public Texture key;
   public string areaToLoad;
   public string areaTransitionName;
   public AreaEntrance theEntrance;
@@ -34,9 +38,43 @@ public class AreaExit : MonoBehaviour
   {
     if (other.tag == "Player")
     {
-      shouldLoadAfterFade = true;
-      UIFade.instance.FadeToBlack();
-      PlayerController.instance.areaTransitionName = areaTransitionName;
+      if (PlayerController.instance.gamestate < 5 && areaTransitionName == "Downstairs5to6")
+      {
+        if (PlayerController.instance.Inventory.Contains(key))
+        {
+          PlayerController.instance.gamestate = 5;
+          keySuccess.Play();
+          shouldLoadAfterFade = true;
+          UIFade.instance.FadeToBlack();
+          PlayerController.instance.areaTransitionName = areaTransitionName;
+        }
+        else
+        {
+          StartCoroutine(DoorLocked());
+          keyFail.Play();
+        }
+      }
+      else
+      {
+        shouldLoadAfterFade = true;
+        UIFade.instance.FadeToBlack();
+        PlayerController.instance.areaTransitionName = areaTransitionName;
+      }
+    }
+  }
+
+  IEnumerator DoorLocked()
+  {
+    lockedMessageTimer = true;
+    yield return new WaitForSeconds(3);
+    lockedMessageTimer = false;
+  }
+
+  public void OnGUI()
+  {
+    if (lockedMessageTimer)
+    {
+      GUI.Box(new Rect(Screen.width / 4, 2 * Screen.height / 3, Screen.width / 2, Screen.height / 3), "Seems I need a key...");
     }
   }
 }
--- Assets/Scripts/BookShelf.cs
+++ Assets/Scripts/BookShelf.cs
@@ -1,34 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BookShelf : MonoBehaviour
 {
-    public Texture booksprite;
-    public AudioSource soundeffect;
-    public GameObject bookshelf;
-    public PlayerController PlayerScript;
-    // Start is called before the first frame update
-    void Start()
+  public Texture booksprite;
+  public AudioSource soundeffect;
+  public GameObject bookshelf;
+  public PlayerController PlayerScript;
+  // Start is called before the first frame update
+  void Start()
+  {
+    PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    if (PlayerScript.gamestate > 2)
     {
-        PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-        if (PlayerScript.gamestate > 2)
-        {
-            bookshelf.SetActive(false);
-        }
-        
+      bookshelf.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (PlayerScript.gamestate > 2)
-        {
-            bookshelf.SetActive(false);
-        }
-    }
+  }
 
-    void OnTriggerStay2D(Collider2D other)
+  // Update is called once per frame
+  void Update()
+  {
+    if (PlayerScript.gamestate > 2)
+    {
+      bookshelf.SetActive(false);
+    }
+  }
+
+  void OnTriggerStay2D(Collider2D other)
   {
     if (other.tag == "Player")
     {
@@ -38,6 +39,8 @@ public class BookShelf : MonoBehaviour
         soundeffect.Play();
         PlayerScript.Inventory.Remove(booksprite);
         bookshelf.SetActive(false);
+        PlayerScript.areaTransitionName = null;
+        SceneManager.LoadScene("CutsceneUpstairs");
       }
     }
   }
