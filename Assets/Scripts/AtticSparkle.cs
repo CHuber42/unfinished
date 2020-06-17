c7c4b50a16973a169a990372e8f6f686fe7dc51b
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-16 21:01:41 -0700

    add bookshelf to attic logic from Chris

---
+++ Assets/Scripts/AtticSparkle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtticSparkle : MonoBehaviour
+{
+    public AudioSource soundEffect;
+    public GameObject sparkle;
+    public PlayerController PlayerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        if (PlayerScript.gamestate > 3)
+        {
+            sparkle.SetActive(false);
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerScript.gamestate > 3)
+        {
+            sparkle.SetActive(false);
+        }
+    }
+
+     void OnTriggerStay2D(Collider2D other)
+  {
+    if (other.tag == "Player")
+    {
+      if (Input.GetKeyDown("j") && PlayerScript.gamestate < 4)
+      {
+        soundEffect.Play();
+        PlayerScript.gamestate = 4;
+      }
+    }
+  }
+}
--- Library/Collab/Original/Assets/Scripts/TilesEvent.cs
+++ Library/Collab/Original/Assets/Scripts/TilesEvent.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class TilesEvent : MonoBehaviour
 {
-  public bool visible = false;
+  public bool visible1 = false;
+  public PlayerController PlayerScript;
+  public SpriteRenderer atticSprite;
 
+  public SpriteRenderer atticSprite2;
+  public Sprite blueSpriteThree;
+
+  public Sprite redSpriteThree;
   // Start is called before the first frame update
   void Start()
   {
-
+    PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    if (PlayerScript.atticTile1 == true)
+    {
+      atticSprite.sprite = blueSpriteThree;
+    }
   }
 
   // Update is called once per frame
@@ -22,7 +32,11 @@ public class TilesEvent : MonoBehaviour
   {
     if (other.tag == "Player")
     {
-      visible = true;
+      visible1 = true;
+      var player = other.GetComponentInParent<PlayerController>();
+      player.atticTile1 = true;
+      atticSprite.sprite = blueSpriteThree;
+      atticSprite2.sprite = redSpriteThree;
     }
   }
 
@@ -30,15 +44,20 @@ public class TilesEvent : MonoBehaviour
   {
     if (other.tag == "Player")
     {
-      visible = false;
+      visible1 = false;
     }
   }
 
   void OnGUI()
   {
-    if (visible)
+    if (visible1)
     {
-      GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "There are some loose tiles here...");
+      GUIStyle myBoxStyle = new GUIStyle(GUI.skin.box);
+      myBoxStyle.fontSize = 20;
+      Font myFont = (Font)Resources.Load("../Fonts/Pixelnauts.ttf", typeof(Font));
+      myBoxStyle.font = myFont;
+      myBoxStyle.normal.textColor = Color.white;
+      GUI.Box(new Rect(Screen.width / 4, 2 * Screen.height / 3, Screen.width / 2, Screen.height / 3), "There are some loose tiles here...", myBoxStyle);
     }
   }
 }
